14a77ac487a15bbbdb5c2955009a88850b805c6f
Suraj ji <rawatsuraj9002@gmail.com>
2025-08-17 13:09:27 +0530

    max sum of sub array

---
+++ DSA/Array/Dsa5_MaxSubArr.java
@@ -0,0 +1,23 @@
+public class Dsa5_MaxSubArr{
+    static int MaxSum(int num[]){
+                int max=Integer.MIN_VALUE; // declare a minimum value 
+        for(int i=0;i<num.length;i++){
+            for(int j=i;j<num.length;j++){
+                int SubArr=0;
+                for(int k=i;k<=j;k++){
+                    SubArr+=num[k];  //Adding value sub array in sub array;
+                }
+                if(SubArr>max){
+                    max=SubArr; // comparing value to get the max sum of sub array;
+                }
+            }
+        }
+        return max;
+    }
+    public static void main(String[]args){
+        int num[]={2,4,6,8,10};
+        System.out.println( "The max value of sub array is " + MaxSum(num));
+
+
+    }
+}
