878f13a157da6e4d9adf0fc4b523d34cc510250e
Suraj ji <rawatsuraj9002@gmail.com>
2025-09-19 17:58:42 +0530

    Counting Sorting

--- DSA/Sorting/sorting.java
+++ DSA/Sorting/sorting.java
@@ -29,28 +29,53 @@ public class sorting{
     // }
 
     //Insertion sort
-    static void insertion(int num[]){
-    for(int i=1;i<num.length;i++){
-        int curr=num[i];
-        int prev=i-1;
-        while(prev>=0 && num[prev]>curr){
-            num[prev+1]=num[prev];
-            prev--;
+    // static void insertion(int num[]){
+    // for(int i=1;i<num.length;i++){
+    //     int curr=num[i];
+    //     int prev=i-1;
+    //     while(prev>=0 && num[prev]>curr){
+    //         num[prev+1]=num[prev];
+    //         prev--;
+    //     }
+    //     num[prev+1]=curr;
+    // }
+    // }
+
+    //counting sort
+    static void countingSort(int arr[]){
+        //finding largest term from the array
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest=Math.max(largest,arr[i]);
         }
-        num[prev+1]=curr;
-    }
-    }
 
+        //creating counting array to find the frequency of every array
+        int count[]=new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
 
+        //sorting the original array
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while(count[i]>0){
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
 
 
     
     public static void main(String[]args){
         int num[]={4,2,1,5,3};
+        int arr[]={2,2,1,3,1,4,6,5,7};
         // bubble(num);
         // selection(num);
-        insertion(num);
-        for(int elem:num){
+        // insertion(num);
+        countingSort(arr);
+        for(int elem:arr){
             System.out.print(elem + " ");
         }
     }
