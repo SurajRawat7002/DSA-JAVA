9f98364263a8800f62a054317d02d6bc30a2f562
Suraj ji <rawatsuraj9002@gmail.com>
2025-10-19 15:58:53 +0530

    string equal function

---
+++ DSA/String/String_5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class String_5{
+    static String subString(String str, int si, int ei){
+        String subStr="";
+        for(int i=si;i<ei;i++){
+            subStr+=str.charAt(i);
+        }
+        return subStr;
+    }
+    public static void main(String[]args){
+//substring 
+String str="Hello World";
+System.out.println(subString(str,0,5));
+    }
+}
