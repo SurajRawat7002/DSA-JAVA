d8445581bce3482c761c437569e07dcf5abbbf39
Suraj ji <rawatsuraj9002@gmail.com>
2025-10-24 21:13:27 +0530

    Basic intro

---
+++ DSA/Bit Manipulation/Bit_1.java
@@ -0,0 +1,30 @@
+class Bit_1{
+    public static void main(String[]args){
+        //Bits 0 and 1
+        // **********Bitwise operator************ 
+
+        // 1 Binary & 
+        System.out.println(4 & 6);
+
+        // 2 Binary or
+
+        System.out.println(4 | 6);
+
+        // 3 Binary XOR
+        System.out.println(5^6^3^2);
+
+        // 4 Binary Ones compliment ~ NOT
+
+        // System.out.println(~5);
+        // System.out.println(~0);
+
+        // 5 Binary left shift <<
+
+        // System.out.println(5<<2);
+
+
+        // 6 Binary Right shift >>
+        System.out.println(6<<2);
+
+    }
+}
